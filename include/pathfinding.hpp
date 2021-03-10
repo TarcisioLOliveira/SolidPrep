5946c5e778ab84f929c52268e755966389988ea1
Tarcísio L Oliveira <devtarc@gmail.com>
2021-03-09 21:47:16 -0300

    Add abstract Pathfinding class
    In order to handle pathfinding in a more modular way, allowing for other
    algorithms to be implemented and chosen.

---
+++ include/pathfinding.hpp
@@ -0,0 +1,40 @@
+/*
+ *   Copyright (C) 2021 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   Foobar is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Foobar is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef PATHFINDING_HPP
+#define PATHFINDING_HPP
+
+#include <gp_Pnt.hxx>
+#include <vector>
+#include <TopoDS_Shape.hxx>
+
+struct PathPoint{
+    gp_Pnt point;
+    double cost;
+    PathPoint* prev;
+};
+
+class Pathfinding{
+    public:
+    virtual ~Pathfinding();
+
+    virtual std::vector<gp_Pnt> find_path(gp_Pnt p, TopoDS_Shape dest) = 0;
+};
+
+#endif
