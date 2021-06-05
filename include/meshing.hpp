4609655db324163e54901516d8741e70a56e4226
Tarcísio L Oliveira <devtarc@gmail.com>
2021-06-04 22:14:38 -0300

    Add function to generate vectors ready for FEM
    Creates MeshElement instances from the list of ElementShapes gotten from
    mesh(). Assumes you are using the same instance.

--- include/meshing.hpp
+++ include/meshing.hpp
@@ -24,14 +24,57 @@
 #include <TopoDS_Shape.hxx>
 #include "element.hpp"
 #include <vector>
+#include "support.hpp"
+#include "force.hpp"
+#include "element_factory.hpp"
+
+class ProjectData;
 
 class Meshing{
     public:
 
+    /**
+     * Just generates a mesh from a shape according to the child class's input
+     * parameters set in its construction. Return MeshNodes are just pointers
+     * to the ones stored in the class object.
+     *
+     * @param s OCCT shape to be meshed
+     * @return collection of ElementShapes (which are just a collection of
+     *         MeshNodes
+     */
     virtual std::vector<ElementShape> mesh(TopoDS_Shape s) = 0;
+    /**
+     * Takes a mesh and boundary conditions and returns a collection of
+     * elements and its load vector, to be used later in a FiniteElement
+     * object.
+     *
+     * Assumes you are using the same instace which generated base_mesh (for
+     * simplicity).
+     *
+     * As it returns two vector instances, both are returned as function
+     * arguments (final_mesh and load_vector).
+     *
+     * The object also stores the mesh elements created.
+     *
+     * @param base_mesh Mesh obtained from mesh()
+     * @param element_type Type of element to be used
+     * @param supports List of supports
+     * @param force List of forces
+     * @param final_mesh Output element list
+     * @param load_vector Output external force vector
+     */
+    virtual void prepare_for_FEM(const std::vector<ElementShape>& base_mesh,
+                                 MeshElementFactory::MeshElementType element_type,
+                                 ProjectData* data,
+                                 std::vector<MeshElement*>& final_mesh,
+                                 std::vector<double>& load_vector);
 
     protected:
     std::vector<std::unique_ptr<MeshNode>> node_list;
+    std::vector<std::unique_ptr<MeshElement>> element_list;
+
+    std::vector<long> get_support_dof(size_t& offset, size_t id, const Support& support, MeshElementFactory::MeshElementType type) const;
+    std::vector<double> get_force_dof(const Force& force, MeshElementFactory::MeshElementType type) const;
 };
 
 #endif
---
+++ src/meshing.cpp
@@ -0,0 +1,146 @@
+/*
+ *   Copyright (C) 2021 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   Foobar is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Foobar is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "meshing.hpp"
+#include "logger.hpp"
+#include "utils.hpp"
+#include "project_data.hpp"
+
+
+void Meshing::prepare_for_FEM(const std::vector<ElementShape>& base_mesh,
+                              MeshElementFactory::MeshElementType element_type,
+                              ProjectData* data,
+                              std::vector<MeshElement*>& final_mesh,
+                              std::vector<double>& load_vector){
+    logger::log_assert(this->node_list.size() > 0, logger::ERROR, "the object's node list is empty. Ensure you are using the same Meshing instance as the one used to obtain the list of ElementShape instances");
+    auto test = base_mesh[0].nodes[0];
+    bool correct = false;
+    for(auto& n : this->node_list){
+        if(n.get() == test){
+            correct = true;
+            break;
+        }
+    }
+    logger::log_assert(correct, logger::ERROR, "object mismatch. Please ensure that the Meshing instance used to generate the list of ElementShape instances is the same as the one being used to prepare the mesh for FEM.");
+
+    this->element_list.clear();
+    this->element_list.reserve(base_mesh.size());
+    final_mesh.reserve(base_mesh.size());
+
+    size_t dof = MeshElementFactory::get_dof_per_node(element_type);
+    size_t k_size = MeshElementFactory::get_k_dimension(element_type);
+    size_t offset = 0;
+
+    load_vector.resize(this->node_list.size()*dof);
+
+    long node_qnt = 0;
+    for(auto& e : base_mesh){
+        std::vector<long> u_pos(k_size);
+        for(size_t i = 0; i < e.nodes.size(); ++i){
+            auto& n = e.nodes[i];
+            node_qnt = std::max(n->id, node_qnt);
+            for(size_t j = 0; j < dof; ++j){
+                u_pos[i*dof + j] = n->id*dof - offset + j;
+            }
+            for(auto& s : data->supports){
+                if(s.S.is_inside(n->point)){
+                    std::vector<long> sup_pos = this->get_support_dof(offset, n->id, s, element_type);
+                    for(size_t j = 0; j < dof; ++j){
+                        u_pos[i*dof + j] = sup_pos[j];
+                    }
+                    break;
+                }
+            }
+            bool checked = false;
+            for(auto& f : data->forces){
+                if(f.S.is_inside(n->point)){
+                    if(!checked){
+                        bool filled = false;
+                        for(size_t j = 0; j < dof; ++j){
+                            if(u_pos[i*dof + j] >= 0){
+                                if(load_vector[u_pos[i*dof + j]] != 0){
+                                    filled = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if(filled){
+                            break;
+                        }
+                        checked = true;
+                    }
+                    std::vector<double> f_vec = this->get_force_dof(f, element_type);
+                    for(size_t j = 0; j < dof; ++j){
+                        if(u_pos[i*dof + j] >= 0){
+                            load_vector[u_pos[i*dof + j]] = f_vec[j];
+                        }
+                    }
+                }
+            }
+        }
+
+        this->element_list.emplace_back(MeshElementFactory::make_element(element_type, u_pos, e, data));
+        final_mesh.push_back(this->element_list.back().get());
+    }
+}
+
+std::vector<long> Meshing::get_support_dof(size_t& offset, size_t id, const Support& support, MeshElementFactory::MeshElementType type) const{
+    size_t size = MeshElementFactory::get_dof_per_node(type);
+    utils::ProblemType prob_type = MeshElementFactory::get_problem_type(type);
+    std::vector<long> pos(size);
+    id *= size;
+    switch(size){
+        case 6:
+            pos[5] = support.MZ ? -1 : (id + offset++);
+            pos[4] = support.MY ? -1 : (id + offset++);
+            pos[3] = support.MX ? -1 : (id + offset++);
+        case 3:
+            if(prob_type == utils::PROBLEM_TYPE_2D){
+                pos[2] = support.MZ ? -1 : (id + offset++);
+            } else {
+                pos[2] = support.Z ? -1 : (id + offset++);
+            }
+        case 2:
+            pos[1] = support.Y ? -1 : (id + offset++);
+            pos[0] = support.X ? -1 : (id + offset++);
+    }
+
+    return pos;
+}
+
+std::vector<double> Meshing::get_force_dof(const Force& force, MeshElementFactory::MeshElementType type) const{
+    size_t size = MeshElementFactory::get_dof_per_node(type);
+    utils::ProblemType prob_type = MeshElementFactory::get_problem_type(type);
+    std::vector<double> f(size);
+    switch(size){
+        case 6:
+        case 3:
+            if(prob_type == utils::PROBLEM_TYPE_3D){
+                f[2] = -force.vec.Z();
+            }
+        case 2:
+            f[1] = -force.vec.Y();
+            f[0] = -force.vec.X();
+    }
+
+
+
+    return f;
+}
