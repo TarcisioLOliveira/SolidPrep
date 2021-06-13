7746ba068e33e16870d9106281d7f33ba8abb821
Tarcísio L Oliveira <devtarc@gmail.com>
2021-06-13 20:53:49 -0300

    Fix utils::to_band()
    Now properly considers cases in which i < j.

--- include/utils.hpp
+++ include/utils.hpp
@@ -134,8 +134,11 @@ namespace utils{
     inline size_t to_band(size_t i, size_t j, int w){
         // lower triangle
         // i >= j
-        size_t diff = (i > j) ? i-j : j-i;
-        return diff*w + j;
+        if(i > j){
+            return (i-j)*w + j;
+        } else {
+            return (j-i)*w + i;
+        }
     }
 
     /**
