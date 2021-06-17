a4e7a01f68b66b5581ab19a6049ba2e8f0512dce
Tarcísio L Oliveira <devtarc@gmail.com>
2021-06-16 22:24:12 -0300

    Add basic topology optimization interface

---
+++ include/topology_optimization.hpp
@@ -0,0 +1,36 @@
+/*
+ *   Copyright (C) 2021 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   Foobar is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Foobar is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TOPOLOGY_OPTIMIZATION_HPP
+#define TOPOLOGY_OPTIMIZATION_HPP
+
+#include <TopoDS_Shape.hxx>
+#include "finite_element.hpp"
+#include "visualization.hpp"
+
+class TopologyOptimization{
+    public:
+
+    virtual TopoDS_Shape optimize(Visualization* viz, FiniteElement* fem, const std::vector<MeshElement*>& mesh, const std::vector<float>& loads) = 0;
+
+};
+
+
+#endif
