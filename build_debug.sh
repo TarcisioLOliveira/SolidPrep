94280a60118e5183f96753e272bfb69ef1997682
Tarcísio L Oliveira <tarcisio.oliveira@usp.br>
2022-04-20 22:48:25 -0300

    Add mkdir -p to build scripts
    Automates creation of debug/release folders

--- build_debug.sh
+++ build_debug.sh
@@ -1,2 +1,3 @@
+mkdir -p debug
 cd debug
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug .. -DCMAKE_INSTALL_PREFIX=. && make install
--- build_release.sh
+++ build_release.sh
@@ -1,2 +1,3 @@
+mkdir -p release
 cd release
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_INSTALL_PREFIX=. && make install
