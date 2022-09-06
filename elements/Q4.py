9d0d2a75d4ac65f2d1a052b7bcb1e56bb2d138c6
Tarcísio L Oliveira <tarcisio.oliveira@usp.br>
2022-09-06 14:53:58 -0300

    Add script to generate Q4
    Still needs some cleaning up to do and testing to do, but everything
    looks ok. Unfornately, some calculations will need to be left to be done
    numerically at runtime, as the symbolic equivalent proved too expensive.
    That will result in some memory overhead to avoid CPU overhead, but it
    should be ok.

---
+++ elements/Q4.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python3
+
+import numpy as np
+import sympy
+from sympy.core.function import *
+import sympy.physics.vector as spv
+import sys
+import re
+
+xi = sympy.symbols('xi')
+eta = sympy.symbols('eta')
+N = [0]*4
+t = sympy.symbols("t")
+D = []
+B = sympy.Matrix(np.zeros((3, 8)))
+J = 0
+a = sympy.symbols('a:4')# [0]*4
+b = sympy.symbols('b:4')# [0]*4
+c = sympy.symbols('c:4')# [0]*4
+d = sympy.symbols('d:4')# [0]*4
+x, y = sympy.symbols('x, y')
+
+def init_N():
+    """ 
+        Initializes the interpolation matrix N.
+    """
+    global N
+
+    # M = sympy.Matrix([[1, x[0], y[0], x[0]*y[0]],
+    #                   [1, x[1], y[1], x[1]*y[1]],
+    #                   [1, x[2], y[2], x[2]*y[2]],
+    #                   [1, x[3], y[3], x[3]*y[3]]]).inv(method="LU")
+    # # M = sympy.simplify(M)
+    # sympy.pprint(M)
+
+    for i in range(4):
+        # truth = np.zeros(4)
+        # truth[i] = 1
+        # truth = sympy.Matrix(truth)
+        # v = M*truth
+        # a[i] = v[0]
+        # b[i] = v[1]
+        # c[i] = v[2]
+        # d[i] = v[3]
+
+        N[i] = a[i] + b[i]*x + c[i]*y + d[i]*x*y
+
+    # N[0] = 0.25*(1-xi)*(1-eta)
+    # N[1] = 0.25*(1+xi)*(1-eta)
+    # N[2] = 0.25*(1+xi)*(1+eta)
+    # N[3] = 0.25*(1-xi)*(1+eta)
+
+def init_DB():
+    """ 
+        Initializes the linear elasticity matrix B and the constitutive matrix D.
+    """
+    global B
+    global D
+    global J
+
+    init_N()
+
+    for i in range(4):
+        dNidx = sympy.diff(N[i], x)
+        dNidy = sympy.diff(N[i], y)
+        B[0, 2*i] = dNidx
+        B[1, 2*i + 1] = dNidy
+        B[2, 2*i] = dNidy
+        B[2, 2*i + 1] = dNidx
+
+    d = sympy.symbols('D:9')
+    D = sympy.Matrix([[d[0], d[1], d[2]],
+                      [d[3], d[4], d[5]],
+                      [d[6], d[7], d[8]]])
+
+def make_Nf():
+    """
+        Creates the Nf matrix.
+    """
+    init_N()
+
+    NN1 = [N[0],   0 , N[1],   0 , N[2],   0 , N[2],   0 ]
+    NN2 = [  0 , N[0],   0 , N[1],   0 , N[2],   0 , N[2]] 
+    NN = t*sympy.Matrix([NN1, NN2]).T
+
+    # Set up variables for line integral
+    x0, x1, y0, y1 = sympy.symbols("x0 x1 y0 y1")
+
+    M = spv.ReferenceFrame("M")
+
+    s = sympy.symbols("s")
+
+    rx = ((1-s)*x0+s*x1)
+    ry = ((1-s)*y0+s*y1)
+    dr = (x1-x0)*M.x + (y1-y0)*M.y
+
+    drnorm = sympy.sqrt((x1-x0)**2 + (y1-y0)**2)
+
+    print("std::vector<double> Nf{")
+    for i in range(len(NN)):
+        # Apply line integration
+        NN[i] = NN[i].subs({x:rx, y:ry})
+        NN[i] = NN[i]*drnorm
+        NN[i] = NN[i].integrate((s, 0, 1))
+
+
+        # Prepare for printing
+        NN[i] = sympy.simplify(sympy.nsimplify(NN[i], rational=True))
+
+        # Format output for use with C++
+        formatted = str(NN[i])
+        formatted = re.sub(r"([abcxy]\d)\*\*2", r"\1*\1", formatted)
+        formatted = re.sub(r"([abcxy])(\d)", r"\1[\2]", formatted)
+
+        formatted = formatted.replace("sqrt", "std::sqrt")
+
+        if i > 0:
+            print(",")
+        print(formatted)
+    print("};")
+
+def make_DB():
+    """
+        Creates the DB matrix.
+    """
+    init_DB()
+
+    DB = D*B
+
+    print("std::vector<double> DB{")
+    for i in range(len(DB)):
+        # Prepare for printing
+        DB[i] = sympy.simplify(sympy.nsimplify(sympy.collect(sympy.expand(DB[i]), [x, y])), rational=True)
+        # Format output for use with C++
+        formatted = str(DB[i])
+        formatted = re.sub(r"([abcD])(\d)", r"\1[\2]", formatted)
+
+        if i > 0:
+            print(",")
+        print(formatted)
+    print("};")
+
+def make_k():
+    """
+        Creates the k matrix.
+    """
+    init_DB()
+
+    # Create the non-integrated matrix
+    k = t*B.T*D*B
+
+    print("std::vector<double> k{")
+    for i in range(len(k)):
+        # Prepare for integration
+        k[i] = sympy.simplify(sympy.collect(sympy.expand(k[i]), [x, y]))
+
+
+        # The integration step
+        k[i] = k[i].integrate((x, -1, 1))
+        k[i] = k[i].integrate((y, -1, 1))
+
+        k[i] = sympy.simplify(sympy.nsimplify(sympy.expand(k[i]), rational=True))
+
+        # Format output for use with C++
+        formatted = str(k[i])
+        formatted = re.sub(r"([abcdD]\d)\*\*2", r"\1*\1", formatted)
+        formatted = re.sub(r"([abcdD])(\d)", r"\1[\2]", formatted)
+
+        if i > 0:
+            print(",")
+        print(formatted)
+    print("};")
+
+def main():
+    # Backwards compatible switch statement
+    args = {
+        "-k":  make_k,
+        "-DB": make_DB,
+        "-Nf": make_Nf
+    }
+    for i in range(1, len(sys.argv)):
+        args[sys.argv[i]]()
+
+
+if __name__ == "__main__":
+    main()
