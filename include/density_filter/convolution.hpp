06e837bf58846d7dfbf151a15766731f0f046fe7
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2022-11-08 13:08:45 -0300

    DensityFilter: add stub Convolution class

---
+++ include/density_filter/convolution.hpp
@@ -0,0 +1,46 @@
+/*
+ *   Copyright (C) 2022 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   SolidPrep is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SolidPrep is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef CONVOLUTION_HPP
+#define CONVOLUTION_HPP
+
+#include "density_filter.hpp"
+
+namespace density_filter{
+
+class Convolution : public DensityFilter{
+    public:
+    Convolution(const double radius);
+
+    virtual ~Convolution() = default;
+
+    virtual void initialize(const Meshing* const mesh) = 0;
+
+    virtual std::vector<double> filter_densities(const std::vector<double>& x) = 0;
+
+    virtual std::vector<double> filter_gradient(const std::vector<double>& df) = 0;
+
+    private:
+
+};
+
+}
+
+#endif
