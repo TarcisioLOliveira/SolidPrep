bd67468c49ae4a2822ce064180f57345f4715ef5
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2022-11-17 16:40:04 -0300

    include: add Projection interface

---
+++ include/projection.hpp
@@ -0,0 +1,38 @@
+/*
+ *   Copyright (C) 2022 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   SolidPrep is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SolidPrep is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PROJECTION_HPP
+#define PROJECTION_HPP
+
+#include <cstddef>
+#include <vector>
+
+class Projection{
+    public:
+    virtual ~Projection() = default;
+    
+    virtual void update(const size_t iteration) = 0;
+
+    virtual void project_densities(const std::vector<double>& x, std::vector<double>& new_x) const = 0;
+
+    virtual void project_gradient(const std::vector<double>& df, std::vector<double>& new_df) const = 0;
+};
+
+#endif
