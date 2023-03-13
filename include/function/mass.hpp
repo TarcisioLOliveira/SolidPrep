31e2fc586b0eb5aae3dba1f4e4929b6882afeb93
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2023-03-13 15:33:03 -0300

    Function: add Mass

---
+++ include/function/mass.hpp
@@ -0,0 +1,48 @@
+/*
+ *   Copyright (C) 2023 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   SolidPrep is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SolidPrep is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef FUNCTION_MASS_HPP
+#define FUNCTION_MASS_HPP
+
+#include "function.hpp"
+#include "meshing.hpp"
+
+namespace function{
+
+class Mass : public DensityBasedFunction{
+    public:
+    Mass(const Meshing* const mesh);
+
+    virtual ~Mass() = default;
+
+    virtual void initialize(const Optimizer* const op) override;
+    virtual double calculate(const Optimizer* const op, const std::vector<double>& u, const std::vector<double>& x) override;
+    virtual double calculate_with_gradient(const Optimizer* const op, const std::vector<double>& u, const std::vector<double>& x, std::vector<double>& grad) override;
+    virtual size_t additional_steps() const override{
+        return 0;
+    }
+
+    private:
+    const Meshing* const mesh;
+};
+
+}
+
+#endif
---
+++ src/function/mass.cpp
@@ -0,0 +1,86 @@
+/*
+ *   Copyright (C) 2023 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   SolidPrep is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SolidPrep is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "function/mass.hpp"
+#include <mpich-x86_64/mpi.h>
+#include <numeric>
+
+namespace function{
+
+Mass::Mass(const Meshing* const mesh):
+    mesh(mesh){}
+
+void Mass::initialize(const Optimizer* const op){
+}
+
+double Mass::calculate(const Optimizer* const op, const std::vector<double>& u, const std::vector<double>& x){
+    (void)u;
+    int mpi_id = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_id);
+
+    if(mpi_id != 0){
+        return 0;
+    }
+    double V = 0;
+    auto v = op->get_volumes();
+
+    auto x_it = x.cbegin();
+    auto v_it = v.cbegin();
+    for(auto& g:this->mesh->geometries){
+        if(g->do_topopt){
+            for(size_t i = 0; i < g->mesh.size(); ++i){
+                auto d = g->materials.get_density(x_it, g->with_void);
+                V += d*(*v_it);
+            }
+        }
+    }
+    V /= 1e9;
+
+    return V;
+}
+double Mass::calculate_with_gradient(const Optimizer* const op, const std::vector<double>& u, const std::vector<double>& x, std::vector<double>& grad){
+    (void)u;
+    int mpi_id = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_id);
+
+    if(mpi_id != 0){
+        return 0;
+    }
+    double V = 0;
+    auto v = op->get_volumes();
+
+    auto x_it = x.cbegin();
+    auto v_it = v.cbegin();
+    auto grad_it = grad.begin();
+    for(auto& g:this->mesh->geometries){
+        if(g->do_topopt){
+            for(size_t i = 0; i < g->mesh.size(); ++i){
+                auto d = g->materials.get_density_deriv(x_it, g->with_void, grad_it);
+                V += d*(*v_it);
+            }
+        }
+    }
+    V /= 1e9;
+
+    return V;
+}
+
+}
+
