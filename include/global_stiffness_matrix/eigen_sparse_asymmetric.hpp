b0addf09cf5686cb39dc42b035ff0aa7e97ff9e7
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2023-03-20 21:40:01 -0300

    global_stiffness_matrix: add EigenSparseAsymmetric

---
+++ include/global_stiffness_matrix/eigen_sparse_asymmetric.hpp
@@ -0,0 +1,68 @@
+/*
+ *   Copyright (C) 2022 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   SolidPrep is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SolidPrep is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef EIGEN_SPARSE_ASYMMETRIC_HPP
+#define EIGEN_SPARSE_ASYMMETRIC_HPP
+
+#include "meshing.hpp"
+#include <Eigen/SparseCore>
+#include <Eigen/src/SparseCore/SparseMatrix.h>
+
+namespace global_stiffness_matrix{
+
+class EigenSparseAsymmetric{
+    public:
+    const double K_MIN = 1e-6;
+
+    virtual ~EigenSparseAsymmetric() = default;
+
+    virtual void generate(const Meshing * const mesh, const std::vector<double>& density, const double pc, const double psi);
+
+    Eigen::SparseMatrix<double>& get_K() {
+        return K;
+    }
+
+    protected:
+    bool first_time = true;
+    Eigen::SparseMatrix<double> K;
+    size_t W, N;
+
+
+    virtual void calculate_dimensions(const Meshing * const mesh, const std::vector<double>& load);
+
+    virtual void add_geometry(const Meshing * const mesh, const Geometry * const g);
+
+    virtual void add_geometry(const Meshing * const mesh, const Geometry * const g, std::vector<double>::const_iterator& rho, const double pc, const double psi);
+
+    inline void insert_element_matrix(const std::vector<double>& k, const std::vector<long>& pos){
+        const size_t w = pos.size();
+        for(size_t i = 0; i < w; ++i){
+            for(size_t j = 0; j < w; ++j){
+                if(pos[i] > -1 && pos[j] > -1){
+                    K.coeffRef(pos[i], pos[j]) += k[w*i + j];
+                }
+            }
+        }
+    }
+};
+
+}
+
+#endif
