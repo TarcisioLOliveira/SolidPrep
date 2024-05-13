d86ff97b091b1b2e0328010f36ca91b03e61b8ef
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2024-05-13 16:14:08 -0300

    root: add NonlinearSolver base class
    Base class for use in FiniteElement to solve nonlinear problems (namely,
    contact problems, currently).
    
    Also add stub Linear class to determine when problem is actually linear
    instead of nonlinear.

---
+++ include/nonlinear_solver.hpp
@@ -0,0 +1,90 @@
+/*
+ *   Copyright (C) 2024 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   SolidPrep is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SolidPrep is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#ifndef NONLINEAR_SOLVER_HPP
+#define NONLINEAR_SOLVER_HPP
+
+#include <cstddef>
+#include "logger.hpp"
+
+class NonlinearSolver{
+    public:
+    enum class SolverClass{
+        LINEAR,
+        GRADIENT_BASED,
+        HESSIAN_BASED
+    };
+    enum class SolverType{
+        LINEAR,
+        STEEPEST_DESCENT,
+        MMA,
+        NEWTON
+    };
+
+    virtual ~NonlinearSolver();
+
+    virtual void setup(size_t N) = 0;
+
+    /**
+     * LINEAR is a stub object, this should not be called.
+     * GRADIENT_BASED takes all expected parameters and updates x based on
+     * gradient.
+     * HESSIAN_BASED takes the solution to the linear problem of a iteration
+     * in dfdx and updates x based on it, f, and internal parameters.
+     */
+    virtual void update(double* x, double f, const double* dfdx) = 0;
+
+    virtual SolverClass get_class() const = 0; 
+
+    virtual SolverType get_type() const = 0; 
+};
+
+namespace nl_solver{
+
+// Stub class
+class Linear : public NonlinearSolver{
+    public:
+    ~Linear() = default;
+
+    virtual void setup(size_t N){
+        (void)N;
+        logger::log_assert(false, logger::ERROR, "Linear solver does not support nonlinear solver methods");
+    }
+
+    virtual void update(double* x, double f, const double* dfdx){
+        (void)x;
+        (void)f;
+        (void)dfdx;
+        logger::log_assert(false, logger::ERROR, "Linear solver does not support nonlinear solver methods");
+    }
+
+    virtual SolverClass get_class() const{
+        return SolverClass::LINEAR;
+    }
+
+    virtual SolverType get_type() const{
+        return SolverType::LINEAR;
+    }
+};
+
+}
+
+#endif
