9300d31f38c6df0c0c7062b53b9b8f31769a0e61
Tarcísio Ladeia de Oliveira <tarcisio.oliveira@usp.br>
2025-09-18 15:41:12 -0300

    field: add ImplantRegion

---
+++ include/field/implant_region.hpp
@@ -0,0 +1,82 @@
+/*
+ *   Copyright (C) 2025 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   SolidPrep is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SolidPrep is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef IMPLANT_REGION_HPP
+#define IMPLANT_REGION_HPP
+
+#include <vector>
+#include "field.hpp"
+#include "project_specification/data_map.hpp"
+
+namespace field{
+
+class ImplantRegion : public ScalarField {
+    public:
+    virtual ~ImplantRegion() = default;
+    ImplantRegion(const projspec::DataMap& data);
+
+    virtual void generate() override;
+    virtual void initialize_views(Visualization* viz) override;
+    virtual void display_views() const override;
+    virtual double get(const MeshElement* e, const gp_Pnt& p) const override;
+
+    inline virtual SubType get_sub_type() const override{
+        return SubType::DOMAIN;
+    }
+
+    private:
+    static const bool reg;
+    inline double f(const double x) const{
+        double sum = a[0];
+        for(size_t i = 1; i < this->a_len; ++i){
+            sum += a[i]*std::pow(x, i);
+        }
+        return sum;
+    }
+    double get_implant_multiplier(const gp_Pnt& p) const;
+
+    const MeshElementFactory* elem_info;
+    std::vector<Geometry*> geoms;
+    double thickness;
+    gp_Pnt center_1;
+    gp_Pnt center_2;
+    double r1;
+    double r2;
+    gp_Dir normal;
+    double decay_distance;
+    std::vector<double> a;
+    double min_str;
+    std::vector<double> a_orig;
+    size_t a_len;
+    double max_l;
+    ScalarField* density_field;
+
+    Visualization* viz;
+
+    bool show;
+    size_t DIM;
+    size_t NODES_PER_ELEM;
+
+    ViewHandler* density = nullptr;
+};
+
+}
+
+#endif
---
+++ src/field/implant_region.cpp
@@ -0,0 +1,200 @@
+/*
+ *   Copyright (C) 2025 Tarcísio Ladeia de Oliveira.
+ *
+ *   This file is part of SolidPrep
+ *
+ *   SolidPrep is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SolidPrep is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SolidPrep.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "project_data.hpp"
+#include "project_specification/registry.hpp"
+#include "field/implant_region.hpp"
+
+namespace field{
+
+ImplantRegion::ImplantRegion(const projspec::DataMap& data):
+    elem_info(data.proj->topopt_element),
+    r1(data.get_double("r1")),
+    r2(data.get_double("r2")),
+    decay_distance(data.get_double("decay_distance")),
+    a(data.get_array("coefficients")->get_double_array()),
+    min_str(a[0]),
+    a_orig(a),
+    a_len(a.size()){
+    
+    if(!data.get_bool("STUB")){
+        this->center_1 = gp_Pnt(
+            data.get_array("center1")->get_double(0),
+            data.get_array("center1")->get_double(1),
+            data.get_array("center1")->get_double(2)
+        );
+        this->center_2 = gp_Pnt(
+            data.get_array("center2")->get_double(0),
+            data.get_array("center2")->get_double(1),
+            data.get_array("center2")->get_double(2)
+        );
+        this->normal = gp_Vec(center_1, center_2);
+        this->max_l = center_1.Distance(center_2);
+
+        const auto geom_ids = data.get_array("geometries")->get_int_array();
+        this->geoms.reserve(geom_ids.size());
+        for(auto i:geom_ids){
+            if(i >= 0){
+                this->geoms.push_back(data.proj->geometries[i].get());
+            }
+        }
+
+        Field* f = data.proj->fields[data.get_int("density_field")].get();
+        logger::log_assert(f->get_type() == Field::Type::SCALAR, logger::ERROR, "bone material type requires a density field");
+        logger::log_assert(f->get_sub_type() == Field::SubType::DOMAIN, logger::ERROR, "field subtype for density field material must be DOMAIN");
+
+        this->density_field = static_cast<ScalarField*>(f);
+    }
+}
+
+double ImplantRegion::get_implant_multiplier(const gp_Pnt& p) const{
+    const gp_Vec v1(this->center_1, p);
+
+    const double l = v1.Dot(this->normal);
+    const gp_Vec v2 = v1 - l*this->normal;
+    const double dist = v2.Magnitude();
+    if(l >= 0 && l <= max_l){
+        const double r = (r2 - r1)*l/max_l + r1;
+        if(dist < r){
+            return min_str;
+        } else if(dist > r + decay_distance){
+            return 1;
+        } else {
+            const double x = dist - r;
+            return this->f(x);
+        }
+    } else if(l < 0){
+        if(dist <= r1){
+            if(-l > decay_distance){
+                return 1;
+            } else {
+                return this->f(-l);
+            }
+        } else {
+            const double dr = dist - r1;
+            const double dc = std::sqrt(l*l + dr*dr);
+            if(dc > decay_distance){
+                return 1;
+            } else {
+                return this->f(dc);
+            }
+        }
+    } else if(l > max_l){
+        const double dl = l - max_l;
+        if(dist <= r2){
+            if(dl > decay_distance){
+                return 1;
+            } else {
+                return this->f(dl);
+            }
+        } else {
+            const double dr = dist - r2;
+            const double dc = std::sqrt(dl*dl + dr*dr);
+            if(dc > decay_distance){
+                return 1;
+            } else {
+                return this->f(dc);
+            }
+        }
+    }
+    return 1;
+}
+
+void ImplantRegion::generate(){}
+void ImplantRegion::initialize_views(Visualization* viz){
+    if(this->show){
+        this->density = viz->add_view("Apparent Density (With Implant)", spview::defs::ViewType::ELEMENTAL, spview::defs::DataType::DENSITY);
+    }
+}
+void ImplantRegion::display_views() const{
+    if(this->show){
+        std::vector<size_t> ids(this->geoms.size(), 0);
+        size_t elem_num = 0;
+        for(size_t i = 0; i < ids.size(); ++i){
+            ids[i] = this->geoms[i]->id;
+            elem_num += this->geoms[i]->mesh.size();
+        }
+        std::vector<double> elem_rho;
+        elem_rho.reserve(elem_num);
+        for(const auto& g:geoms){
+            for(const auto& e:g->mesh){
+                const auto c = e->get_centroid();
+                const auto m = this->get_implant_multiplier(c);
+                elem_rho.push_back(m*this->density_field->get(e.get(), c));
+            }
+        }
+        this->density->update_view(elem_rho, ids);
+    }
+}
+double ImplantRegion::get(const MeshElement* e, const gp_Pnt& p) const{
+    const auto m = this->get_implant_multiplier(p);
+    const auto rho = this->density_field->get(e, p);
+
+    return m*rho;
+}
+
+using namespace projspec;
+const bool ImplantRegion::reg = Factory<Field>::add(
+    [](const DataMap& data){
+        return std::make_unique<ImplantRegion>(data);
+    },
+    ObjectRequirements{
+        "implant_region",
+        {
+            DataEntry{.name = "density_field", .type = TYPE_INT, .required = true},
+            DataEntry{.name = "geometries", .type = TYPE_ARRAY, .required = true,
+                         .array_data = std::shared_ptr<ArrayRequirements>(
+                             new ArrayRequirements{
+                                 .size = 0,
+                                 .type = TYPE_INT
+                             }
+                         ),
+                     },
+            DataEntry{.name = "center1", .type = TYPE_ARRAY, .required = true,
+                 .array_data = std::shared_ptr<ArrayRequirements>(
+                     new ArrayRequirements{
+                         .size = 3,
+                         .type = TYPE_DOUBLE
+                     }
+                 ),
+            },
+            DataEntry{.name = "center2", .type = TYPE_ARRAY, .required = true,
+                 .array_data = std::shared_ptr<ArrayRequirements>(
+                     new ArrayRequirements{
+                         .size = 3,
+                         .type = TYPE_DOUBLE
+                     }
+                 ),
+            },
+            DataEntry{.name = "r1", .type = TYPE_DOUBLE, .required = true},
+            DataEntry{.name = "r2", .type = TYPE_DOUBLE, .required = true},
+            DataEntry{.name = "decay_distance", .type = TYPE_DOUBLE, .required = true},
+            DataEntry{.name = "coefficients", .type = TYPE_ARRAY, .required = true,
+                 .array_data = std::shared_ptr<ArrayRequirements>(
+                     new ArrayRequirements{
+                         .size = 0,
+                         .type = TYPE_DOUBLE
+                     }
+                 ),
+            },
+        }
+    }
+);
+}
